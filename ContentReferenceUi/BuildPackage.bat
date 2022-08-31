7df1cc996c589746baec5e662d05bb4813bbe088
Mike Wills <mwills@bluemodus.com>
2022-08-30 22:04:11 -0700

    Renamed namespace back to KenticoCommunity, because that's already the name of the module given pre-release to Kentico.

--- ContentReferenceUi/BuildPackage.bat
+++ ContentReferenceUi/BuildPackage.bat
@@ -1,6 +1,6 @@
 del *.nupkg
 dotnet build .\ContentReferenceUi.csproj -c Release
 nuget pack ContentReferenceUi.csproj -Prop Configuration=Release
-copy .\XperienceCommunity.ContentReferenceUi.*.nupkg C:\_OfflineNugetSource\
+copy .\KenticoCommunity.ContentReferenceUi.*.nupkg C:\_OfflineNugetSource\
 @echo off
 pause
