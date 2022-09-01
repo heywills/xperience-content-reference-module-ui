2e61cef8239556c262c7ea373d570f8d8217dccb
Mike Wills <mwills@bluemodus.com>
2022-08-31 17:31:26 -0700

    ContentReferenceUniGridExtender is working.

--- ContentReferenceUi/Extenders/ContentReferenceUniGridExtender.cs
+++ ContentReferenceUi/Extenders/ContentReferenceUniGridExtender.cs
@@ -5,6 +5,7 @@ using CMS.Core;
 using CMS.DocumentEngine;
 using CMS.UIControls;
 using KenticoCommunity.ContentReferenceUi.Extenders;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -17,9 +18,21 @@ namespace KenticoCommunity.ContentReferenceUi.Extenders
 {
     public class ContentReferenceUniGridExtender: ControlExtender<UniGrid>
     {
+        private const string CMS_VIEW_LISTING_SCRIPT = "~/CMSModules/Content/CMSDesk/View/Listing.js";
+
         public override void OnInit()
         {
             Control.OnDataReload += Control_OnDataReload;
+            LoadScriptsRequiredForEditAction();
+        }
+
+        private void LoadScriptsRequiredForEditAction()
+        {
+            var page = Control.Page;
+            ScriptHelper.RegisterLoader(page);
+            ScriptHelper.RegisterDialogScript(page);
+            ScriptHelper.RegisterJQuery(page);
+            ScriptHelper.RegisterScriptFile(Control, CMS_VIEW_LISTING_SCRIPT);
         }
 
         private DataSet Control_OnDataReload(string completeWhere,
@@ -30,18 +43,26 @@ namespace KenticoCommunity.ContentReferenceUi.Extenders
                                                    int currentPageSize,
                                                    ref int totalRecords)
         {
-            var currentNode = GetCurrentTreeNode();
-            var contentReferences = GetReferences(currentNode);
-            var contentReferenceGuids = contentReferences.Where(c => c.NodeGuid.HasValue)
-                                                         .Select(c => c.NodeGuid.Value);
+            try
+            {
+                var currentNode = GetCurrentNode();
+                var contentReferences = GetReferences(currentNode);
+                var contentReferenceGuids = contentReferences.Where(c => c.NodeGuid.HasValue)
+                                                             .Select(c => c.NodeGuid.Value);
 
-            var docs = DocumentHelper.GetDocuments()
-                .WhereIn("NodeGuid", contentReferenceGuids.ToList())
-                .OrderBy("NodeAliasPath")
-                .Culture(currentNode.DocumentCulture)
-                .Published(false)
-                .LatestVersion(true);
-            return docs.Result;
+                var docs = DocumentHelper.GetDocuments()
+                    .WhereIn("NodeGuid", contentReferenceGuids.ToList())
+                    .OrderBy("NodeAliasPath")
+                    .Culture(currentNode.DocumentCulture)
+                    .Published(false)
+                    .LatestVersion(true);
+                return docs.Result;
+            }
+            catch(Exception ex)
+            {
+                Control.AddMessage(MessageTypeEnum.Error, $"An unexpected error occured: {ex.Message}");
+            }
+            return null;
         }
 
         private IEnumerable<ContentReference> GetReferences(ITreeNode currentNode)
@@ -50,16 +71,35 @@ namespace KenticoCommunity.ContentReferenceUi.Extenders
             return contentReferenceService.GetParentReferencesByNode(currentNode);
         }
 
-        private ITreeNode GetCurrentTreeNode()
+        private CMSUIPage GetCmsUiPage()
         {
-            var page = base.Control.Page;
-            var cmsPropertyPage = (page is CMSPropertiesPage) ? (CMSPropertiesPage)page : null;
-            if (cmsPropertyPage == null)
+            var page = Control.Page;
+            if(!(page is CMSUIPage))
             {
-                throw new System.Exception("This extender can only be used on a CMSPropertyPage"); // TODO: Improve this.
+                throw new InvalidOperationException($"The {nameof(ContentReferenceUniGridExtender)} can only be used on a Kentico CMS UI page.");
             }
-            return cmsPropertyPage.Node;
+            return (CMSUIPage)page;
         }
 
+        private ITreeNode GetCurrentNode()
+        {
+            var cmsUiPage = GetCmsUiPage();
+            var nodeId = cmsUiPage.NodeID;
+            var cultureCode = cmsUiPage.CultureCode;
+            if (!(nodeId > 0))
+            {
+                throw new ArgumentException($"The nodeId parameter is invalid.");
+            }
+            if (string.IsNullOrWhiteSpace(cultureCode))
+            {
+                throw new ArgumentException($"The culture parameter is invalid.");
+            }
+            var currentNode = DocumentHelper.GetDocument(nodeId, cultureCode, cmsUiPage.Tree);
+            if(currentNode == null)
+            {
+                throw new ArgumentException($"Could not identify the current TreeNode. This is usually because the nodeId or culture parameter is invalid.");
+            }
+            return currentNode;
+        }
     }
 }
