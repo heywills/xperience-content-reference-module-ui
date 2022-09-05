584cc66b98896e0394516b3cfc37095988a3f815
Mike Wills <mwills@bluemodus.com>
2022-09-05 10:23:12 -0700

    Dynamically determine module name

--- ContentReferenceUi/build/post-build.ps1
+++ ContentReferenceUi/build/post-build.ps1
@@ -13,24 +13,30 @@ function GetModuleVersion($assemblyPath)
 	return $moduleVersion
 }
 
-function CreateCorrectlyNamedEmptyExport($projectDir, $moduleVersion)
+function CreateCorrectlyNamedEmptyExport($projectDir, $moduleVersion, $assemblyName)
 {
 	Write-Host "    Creating empty module export file"
-	$templateExportPath = $projectDir + "build\EmptyExport.zip"
-	$targetFolder = ("{0}content\App_Data\CMSModules\KenticoCommunity.ContentReferenceUi\Install\" -f $projectDir)
-	$targetExportPath = ("{0}KenticoCommunity.ContentReferenceUi_{1}.zip" -f $targetFolder, $moduleVersion)
-	if (Test-Path -Path $targetFolder) {
-		Write-Host ("      Deleting contents of: {0}" -f $targetFolder)
-		Remove-Item ("{0}*.*" -f $targetFolder)
+    $projectDirTrimmed = TrimTrailingSlash $projectDir
+	$templateExportPath = $projectDirTrimmed + "\build\EmptyExport.zip"
+	$targetFolderPath = ("{0}\content\App_Data\CMSModules\{1}\Install" -f $projectDirTrimmed, $assemblyName)
+	$targetExportPath = ("{0}\KenticoCommunity.ContentReferenceUi_{1}.zip" -f $targetFolderPath, $moduleVersion)
+	if (Test-Path -Path $targetFolderPath) {
+		Write-Host ("      Deleting contents of: {0}" -f $targetFolderPath)
+		Remove-Item ("{0}\*.*" -f $targetFolderPath)
 	}
     else
     {
-        New-Item $targetFolder -ItemType Directory
+        New-Item $targetFolderPath -ItemType Directory
     }
 	Write-Host ("      Export template: {0}" -f $templateExportPath)
 	Write-Host ("      Target export path: {0}" -f $targetExportPath)
 
 	Copy-Item $templateExportPath $targetExportPath
+}
+
+function TrimTrailingSlash($path)
+{
+    return $path.Trim(@("/","\"))
 }
 
 try {
@@ -40,7 +46,7 @@ try {
 	Write-Host ("    Assembly path: {0}" -f $assemblyPath)
 	$moduleVersion = GetModuleVersion $assemblyPath
 	Write-Host ("    Module version: {0}" -f $moduleVersion)
-	CreateCorrectlyNamedEmptyExport $projectDir $moduleVersion
+	CreateCorrectlyNamedEmptyExport $projectDir $moduleVersion $assemblyName
 
 	exit 0
 }
